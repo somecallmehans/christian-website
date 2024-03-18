430827da3b88bfb8cc9a3e11e406e0f6d80a7ff7
Christian <somecallmehans@gmail.com>
2024-03-18 16:29:12 -0400

    add in animations

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ function App() {
   return (
     <div className="App">
       <div>
-        <div className="App-text-header">Hey! </div>
-        <div className="App-text-header">
-          My name is Christian Anderson (He/Him).
+        <div className="App-text-header fade">Hey! </div>
+        <div className="App-text-header fade delay-1">
+          My name is Christian <span>(He/Him)</span>.
         </div>
-        <div className="App-text-content">
+        <div className="App-text-content fade delay-2">
           I'm a full stack developer, musician, and general handy person.
         </div>
-        <div className="App-text-content">This is my website.</div>
-        <div className="icon-container ">
-          <i className="fa-brands fa-linkedin App-icon" />
+        <div className="App-text-content fade delay-3">This is my website.</div>
+        <div className="icon-container fade delay-4">
+          <i className="fa-brands fa-linkedin App-icon fade-in" />
           <i className="fa-brands fa-square-github App-icon" />
           <i className="fa-brands fa-spotify App-icon" />
           <i className="fa-solid fa-square-envelope App-icon" />
