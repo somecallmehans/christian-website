d78f2bd23dd93ba6766cd4931dd7d161f3d40de7
Christian <somecallmehans@gmail.com>
2024-03-18 15:28:06 -0400

    style and alignment, also for smaller screens

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div style={{ marginLeft: "20px" }}>
-        <div>Hi. </div>
-        <div>My name is Christian Anderson.</div>
-        <div>This is my website.</div>
+      <div>
+        <div className="App-text-header">Hi. </div>
+        <div className="App-text-header">
+          My name is Christian Anderson (He/Him).
+        </div>
+        <div className="App-text-content">
+          I'm a full stack developer, a musician, and general handy person.
+        </div>
+        <div className="App-text-content">This is my website.</div>
+        <div className="icon-container ">
+          <i className="fa-brands fa-linkedin App-icon" />
+          <i className="fa-brands fa-square-github App-icon" />
+          <i className="fa-brands fa-spotify App-icon" />
+          <i className="fa-solid fa-square-envelope App-icon" />
+        </div>
       </div>
     </div>
   );
